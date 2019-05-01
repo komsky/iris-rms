b114c52b818fe8829483ea760ef9c37127ab5f56
Adrian Ilewicz <adrian@iris.net>
2019-05-01 11:03:08 +0100

    feat: not seeding database, we want a fresh empty start

--- DAL/Iris.Rms.Data/DataSeeder.cs
+++ DAL/Iris.Rms.Data/DataSeeder.cs
@@ -17,12 +17,6 @@ namespace Iris.Rms.Data
         public async Task Seed()
         {
             _context.Database.Migrate();
-            if (!_context.RmsList.Any())
-            {
-                _context.RmsList.Add(new Models.RmsConfig { Name = "First and only RMS... for now" });
-
-            }
-
             _context.SaveChanges();
             _context.Database.ExecuteSqlCommand("SET IDENTITY_INSERT dbo.RmsList ON;");
             _context.SaveChanges();
